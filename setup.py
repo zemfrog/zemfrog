64ffd395a2aa2fea70ac5ae5aaad90cc6f3ec8a7
aprilahijriyan <hijriyan23@gmail.com>
2020-09-08 21:23:41 +0100

    🚀 RELEASE: v1.0.4

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zemfrog/zemfrog',
-    version='1.0.3',
+    version='1.0.4',
     zip_safe=False,
 )
