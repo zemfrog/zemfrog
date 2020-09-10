0c153a582555f34456a7ed64669e093fbfc483ca
aprilahijriyan <hijriyan23@gmail.com>
2020-09-10 08:51:53 +0100

    🚀 RELEASE: v1.0.5

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zemfrog/zemfrog',
-    version='1.0.4',
+    version='1.0.5',
     zip_safe=False,
 )
--- zemfrog/__init__.py
+++ zemfrog/__init__.py
@@ -4,4 +4,4 @@ Zemfrog is a simple framework for building web or APIs with flask.
 
 __author__ = "Aprila Hijriyan"
 __email__ = "hijriyan23@gmail.com"
-__version__ = "1.0.4"
+__version__ = "1.0.5"
