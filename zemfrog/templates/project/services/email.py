40e4fd92639efda42006e769377e7d655768fd71
aprilahijriyan <hijriyan23@gmail.com>
2020-09-15 13:58:32 +0700

    🐛 FIX: send async email

--- zemfrog/templates/project/services/email.py
+++ zemfrog/templates/project/services/email.py
@@ -1,9 +1,7 @@
 from extensions.celery import celery
 from extensions.mail import mail
-from flask_mail import Message
 
 
 @celery.task
 def send_email(*args, **kwds):
-    msg = Message(*args, *kwds)
-    mail.send(msg)
+    mail.send_message(*args, **kwds)
