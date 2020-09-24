16b4c48e44da6ebe0d52941f415f2d4f363f8316
aprilahijriyan <hijriyan23@gmail.com>
2020-09-25 02:01:29 +0700

    🐛 FIX: bug on password reset endpoint

--- zemfrog/templates/project/auth/urls.py
+++ zemfrog/templates/project/auth/urls.py
@@ -6,5 +6,5 @@ routes = [
     ("/register", views.register, ["POST"]),
     ("/confirm/<token>", views.confirm_account, ["GET"]),
     ("/forgot-password", views.request_password_reset, ["POST"]),
-    ("/password-reset/<token>", views.password_reset, ["GET"]),
+    ("/password-reset/<token>", views.password_reset, ["POST"]),
 ]
