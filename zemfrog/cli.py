26412f345e073346fd48458f6097fc9e68d228d2
aprilahijriyan <hijriyan23@gmail.com>
2020-10-05 02:55:40 +0700

    💥 NEW: add version option

--- zemfrog/cli.py
+++ zemfrog/cli.py
@@ -1,9 +1,11 @@
 import click
 
+from . import __version__
 from .generator import g_project
 
 
 @click.group()
+@click.version_option(__version__, "-v", "--version", message="Zemfrog v%(version)s")
 def main():
     """
     Zemfrog CLI
