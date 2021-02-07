ae8e2cf6b12e2a9b2635e516d46da92048da7723
aprilahijriyan <hijriyan23@gmail.com>
2021-02-07 12:47:32 +0700

    💥 NEW: Added file upload

---
+++ docs/fileupload.rst
@@ -0,0 +1,17 @@
+File Upload
+===========
+
+For now, uploading files with flask-apispec is a little tricky.
+However, you can still upload files via swagger-ui.
+
+
+All you need is to import ``FileField`` from ``extensions.apispec``. For example::
+
+    from extensions.apispec import FileField
+    from from flask_apispec import use_kwargs
+
+    @use_kwargs({"image": FileField()}, location="files")
+    def view_function(**kwds):
+        image = kwds.get("image")
+        print("Image:", image)
+
--- zemfrog/templates/project/extensions/apispec.py
+++ zemfrog/templates/project/extensions/apispec.py
@@ -6,20 +6,30 @@ from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from flask.app import Flask
 from flask_apispec import FlaskApiSpec
+from marshmallow import fields
 
 docs = FlaskApiSpec(document_options=False)
+FileField = fields.Raw
 
 
 def init_app(app: Flask):
+    global FileField
+
+    ma = MarshmallowPlugin()
+    spec = APISpec(
+        title=app.config.get("APISPEC_TITLE", "API Docs"),
+        version=app.config.get("APISPEC_VERSION", "v1"),
+        openapi_version=app.config.get("APISPEC_OAS_VERSION", "2.0"),
+        plugins=[ma],
+        securityDefinitions=app.config.get("APISPEC_SECURITY_DEFINITIONS", {}),
+    )
+    @ma.map_to_openapi_type('file', None)
+    class FileField(fields.Raw):
+        pass
+
     app.config.update(
         {
-            "APISPEC_SPEC": APISpec(
-                title=app.config.get("APISPEC_TITLE", "API Docs"),
-                version=app.config.get("APISPEC_VERSION", "v1"),
-                openapi_version=app.config.get("APISPEC_OAS_VERSION", "2.0"),
-                plugins=[MarshmallowPlugin()],
-                securityDefinitions=app.config.get("APISPEC_SECURITY_DEFINITIONS", {}),
-            ),
+            "APISPEC_SPEC": spec,
         }
     )
     docs.init_app(app)
