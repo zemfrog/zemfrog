e1973d133b0923c561368dc43a7050fa093e9774
aprilahijriyan <hijriyan23@gmail.com>
2021-02-24 06:50:41 +0700

    ✨ New Feature: add jinja filters loader

---
+++ zemfrog/commands/filter.py
@@ -0,0 +1,24 @@
+import click
+from flask.cli import with_appcontext
+from ..generator import g_filter
+
+
+@click.group("filter")
+def group():
+    """
+    Jinja filter generator.
+    """
+
+
+@group.command()
+@with_appcontext
+@click.argument("name")
+def new(name):
+    """
+    Create a new filter.
+    """
+
+    g_filter(name)
+
+
+command = group
---
+++ zemfrog/templates/filter/name.py
@@ -0,0 +1,2 @@
+def init_filter():
+    return {}
