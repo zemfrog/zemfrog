9f1e533cd09d8b6281dbe44a9edc02b2790fbf31
aprilahijriyan <hijriyan23@gmail.com>
2021-04-11 14:32:08 +0700

    🐛 FIX: no longer support python < 3.7

--- setup.py
+++ setup.py
@@ -31,14 +31,13 @@ project_urls = {
 setup(
     author=__author__,
     author_email=__email__,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Framework :: Flask",
